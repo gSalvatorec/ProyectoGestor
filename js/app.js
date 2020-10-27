e68d4088dbc3afd5a53097e4790428f4896b9a17
gSalvatorec <gsalvadorcr0322@gmail.com>
2020-10-26 23:52:43 -0600

    Agregamos propiedades en css y js que permiten la animacion de una imagen y texto en el header

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let animacion = document.querySelector('#animacion-principal'),
+        animacionT = document.querySelector('#ct1');
+    animacion.style.animation = 'mover 1.5s ease-out';
+    animacionT.style.animation = 'mover2 1.5s ease-out';
+});
