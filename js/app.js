d58ed1f561e564fa71bbf35659ee9a0505dbd8ae
gSalvatorec <gsalvadorcr0322@gmail.com>
2020-10-27 01:22:55 -0600

    Agregamos menu desplegable a medias, faltan correcciones a la hora de cerrarlo Responsive

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ window.addEventListener('DOMContentLoaded', () => {
     animacion.style.animation = 'mover 1.5s ease-out';
     animacionT.style.animation = 'mover2 1.5s ease-out';
 });
+
+const btn_cerrar = document.querySelector('#cerrar'),
+    navegacion = document.querySelector('.navegacion'),
+    nav2 = document.querySelector('#navegacion');
+// console.log(nav2);
+// console.log(nav2.target.classList);
+
+// Agregando evento al presionar btn_cerrar para activar o desactivar menu responsivo 
+btn_cerrar.addEventListener('click', e => {
+    e.preventDefault();
+    if (nav2.classList.contains('navegacion')) {
+        nav2.classList.add('navegacion-activa');
+        nav2.classList.remove('navegacion');
+        nav2.style.animation = 'menu 1s ease-out';
+    } else {
+        nav2.classList.remove('navegacion-activa');
+        nav2.classList.add('navegacion');
+        navegacion.style.animation = 'menu2 1s ease-out';
+    }
+})
