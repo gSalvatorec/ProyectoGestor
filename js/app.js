5d854fbd64709c77ca529089cf06e1be70b4ba06
gSalvatorec <gsalvadorcr0322@gmail.com>
2020-10-27 02:53:05 -0600

    Agregamos la seccion Nosotros y le dimos estilos responsivos por el momento solo para celular, se agregan animaciones a una imagen y texto

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ window.addEventListener('DOMContentLoaded', () => {
     animacion.style.animation = 'mover 1.5s ease-out';
     animacionT.style.animation = 'mover2 1.5s ease-out';
 });
+
+window.addEventListener('scroll', () => {
+    let imagenN = document.querySelector('.imagen-nosotros'),
+        textoN = document.querySelector('#texto-nosotros'),
+        posicionOBJN = imagenN.getBoundingClientRect().top,
+        alturaOBj = window.innerHeight / 3;
+    console.log(posicionOBJN);
+    // console.log(scroll);
+    if (posicionOBJN < alturaOBj) {
+        imagenN.style.opacity = '1';
+        textoN.style.opacity = '1';
+    }
+});
